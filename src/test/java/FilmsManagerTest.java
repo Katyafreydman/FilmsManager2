2a8d4e21af31de3d118977d3fd525b2652314965
Катя Фрейдман <katyafreydman@gmail.com>
2022-09-17 22:03:40 +0300

    Create new tests

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -50,4 +50,55 @@ public class FilmsManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void findLast3() {
+        FilmsManager manager = new FilmsManager(10);
+
+        String[] actual = manager.findLast();
+        String[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastMore() {
+        FilmsManager manager = new FilmsManager(10);
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+        manager.add("film10");
+        manager.add("film11");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2"};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLastTen() {
+        FilmsManager manager = new FilmsManager(10);
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+        manager.add("film10");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
